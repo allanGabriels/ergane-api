2227868f106b2673e752d508645d1f35d10d48b4
henrique-henschel <henriquehenschel16@gmail.com>
2026-06-14 17:34:38 -0300

    Task IX-57 - Início do Desenvolvimento do Back-End da Aplicação

---
+++ src/main/java/com/ergane/api/model/Venda.java
@@ -0,0 +1,36 @@
+package com.ergane.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "vendas")
+public class Venda {
+
+    @MongoId(FieldType.OBJECT_ID)
+    private String id;
+
+    private String nomeCliente;
+    private String cpfCliente;
+    private String metodoPagamento;
+    private BigDecimal valorTotal;
+    private BigDecimal valorRecebido;
+    private BigDecimal troco;
+    private Double latitude;
+    private Double longitude;
+    private List<Produto> itens;
+    private LocalDateTime dataHora;
+
+}
